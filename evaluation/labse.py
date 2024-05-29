4fd9e1f4a330c9765658cee4230baa016550e3ba
Jan Slíva <honzin.sliva@gmail.com>
2024-05-30 01:29:29 +0200

    upgrade baseline methods

--- evaluation/labse.py
+++ evaluation/labse.py
@@ -1,56 +1,19 @@
 import torch
-from transformers import BertModel, BertTokenizerFast
+from sentence_transformers import SentenceTransformer
 
 class Labse:
 
     def __init__(self) -> None:
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.tokenizer = BertTokenizerFast.from_pretrained("setu4993/LaBSE")
-        self.model = BertModel.from_pretrained("setu4993/LaBSE").to(self.device)
-        self.model = self.model.eval()
+        self.model = SentenceTransformer('sentence-transformers/LaBSE')
+        if self.device == "cpu":
+            self.model.cpu()
+        else:
+            self.model.cuda(self.device)
 
-    def predict_transformer(self, sentences, batch_size):
-        inputs = self.tokenizer(sentences, return_tensors="pt", padding=True)
+    def predict(self, sentences, batch_size, verbose=False):
+        return self.model.encode(sentences, batch_size=batch_size, show_progress_bar=verbose)
 
-        emb_dim = self.model.pooler.dense.weight.size()[0]
-        ret = torch.zeros((len(sentences), emb_dim))
-
-        for s in range(0, len(sentences), batch_size):
-            e = min(s+batch_size, len(sentences))
-            inputs_batch = {}
-            for key in inputs.keys():
-                inputs_batch[key] = inputs[key][s:e].to(self.device)
-            with torch.no_grad():
-                ret[s:e] = self.model(**inputs_batch).pooler_output
-
-        return ret.cpu().detach().numpy()
-
-
-    def predict_word_emb(self, sentences, batch_size):
-        inputs = self.tokenizer(sentences, return_tensors="pt", padding=True)
-
-        emb_dim = self.model.embeddings.word_embeddings.embedding_dim
-
-        fast_emb = torch.zeros((len(sentences), emb_dim)).to(self.device)
-        inputs["input_ids"] = inputs["input_ids"].to(self.device)
-
-        for s in range(0, len(sentences), batch_size):
-            print(f"batch no. {s//batch_size}/{(len(sentences)//batch_size) + 1}")
-            e = min(s+batch_size, len(sentences))
-            with torch.no_grad():
-                fast_emb_sent = self.model.embeddings.word_embeddings(inputs["input_ids"][s:e])
-
-            for i in range(fast_emb_sent.shape[0]):
-                fast_emb[s+i] = 0
-                j = 1
-                while inputs["input_ids"][s+i, j] != 102: # 102 = SEP token
-                    fast_emb[s+i] += fast_emb_sent[i, j]
-                    j += 1
-                if (j > 1): fast_emb[s+i] /= (j-1)
-
-            del fast_emb_sent
-
-        return fast_emb.cpu().detach().numpy()
     
 if __name__ == "__main__":
     model = Labse()
@@ -59,5 +22,4 @@ if __name__ == "__main__":
         "Puppies are nice.",
         "I enjoy taking long walks along the beach with my dog.",
     ]
-    print(model.predict_transformer(english_sentences, 2))
-    print(model.predict_word_emb(english_sentences, 2))
+    print(model.predict(english_sentences, 2))
---
+++ evaluation/word_emb.py
@@ -0,0 +1,52 @@
+import torch
+from transformers import BertModel, BertTokenizerFast
+from math import ceil
+
+class WordEmb:
+
+    def __init__(self) -> None:
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tokenizer = BertTokenizerFast.from_pretrained("setu4993/LaBSE")
+        self.model = BertModel.from_pretrained("setu4993/LaBSE").to(self.device)
+        self.model = self.model.eval()
+
+        inputs = self.tokenizer([""], return_tensors="pt", padding=True)
+        inputs["input_ids"] = inputs["input_ids"].to(self.device)
+        inputs["attention_mask"] = inputs["attention_mask"].to(self.device)
+        
+        with torch.no_grad():
+            self.empty_emb = self.model.embeddings.word_embeddings(inputs["input_ids"]).sum(axis=1)
+        self.empty_tokens = inputs["attention_mask"].sum(axis=1)
+
+    def predict(self, sentences, batch_size, verbose = False):
+        inputs = self.tokenizer(sentences, return_tensors="pt", padding=True)
+
+        emb_dim = self.model.embeddings.word_embeddings.embedding_dim
+
+        fast_emb = torch.zeros((len(sentences), emb_dim)).to(self.device)
+        inputs["input_ids"] = inputs["input_ids"].to(self.device)
+        inputs["attention_mask"] = inputs["attention_mask"].to(self.device)
+
+        for s in range(0, len(sentences), batch_size):
+            if (verbose): print(f"batch no. {1 + s//batch_size}/{ceil(len(sentences)/batch_size)}")
+            e = min(s+batch_size, len(sentences))
+            with torch.no_grad():
+                fast_emb_sent = self.model.embeddings.word_embeddings(inputs["input_ids"][s:e])
+
+            fast_emb[s:e] = (fast_emb_sent * inputs["attention_mask"][s:e][:, :, None]).sum(axis=1) - self.empty_emb
+            fast_emb[s:e] = torch.div(fast_emb[s:e], (inputs["attention_mask"][s:e].sum(axis=1) - self.empty_tokens)[:, None])
+
+            del fast_emb_sent
+
+        return fast_emb.cpu().detach().numpy()
+    
+
+    
+if __name__ == "__main__":
+    model = WordEmb()
+    english_sentences = [
+        "dog",
+        "Puppies are nice.",
+        "I enjoy taking long walks along the beach with my dog.",
+    ]
+    print(model.predict(english_sentences, 2, True))
