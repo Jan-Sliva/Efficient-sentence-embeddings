071c2532b99b0acafc13a2468be24ed72ea94eb6
Jan Slíva <honzin.sliva@gmail.com>
2024-07-24 22:28:31 +0200

    evaluation

--- evaluation/evaluate_BUCC.py
+++ evaluation/evaluate_BUCC.py
@@ -3,7 +3,7 @@ import os
 
 import time
 
-from utils_retrieve import bucc_eval, extract_ids_and_sentences, mine_bitext, extract_file_as_list
+from utils_retrieve import bucc_eval, extract_ids_and_sentences, mine_bitext, extract_file_as_list_bucc
 import os.path as P
 
 # BUCC - F1
@@ -40,6 +40,11 @@ def BUCC_eval(input_folder, output_folder, extract_emb_f, save_embs=False, use_g
     if not P.exists(output_folder):
         os.mkdir(output_folder)
 
+    result_file = P.join(output_folder, "results.csv")
+
+    with open(result_file, "w") as f:
+        f.write("pair,time,precision,recall,F1,best-threshold\n")
+
     for pair in BUCC_pairs:
         x_lang = pair.split("-")[0]
         y_lang = pair.split("-")[1]
@@ -50,8 +55,8 @@ def BUCC_eval(input_folder, output_folder, extract_emb_f, save_embs=False, use_g
         output_file = P.join(output_folder, pair, "{}.training.output".format(pair))
         predict_file = P.join(output_folder, pair, "{}.training.predict".format(pair))
 
-        x_list = extract_file_as_list(x_file, mode="bucc")
-        y_list = extract_file_as_list(y_file, mode="bucc")
+        x_list = extract_file_as_list_bucc(x_file)
+        y_list = extract_file_as_list_bucc(y_file)
 
         pair_output_dir = P.join(output_folder, pair)
         if not P.exists(pair_output_dir):
@@ -69,48 +74,12 @@ def BUCC_eval(input_folder, output_folder, extract_emb_f, save_embs=False, use_g
             np.save(emb_y_file, y)
 
         vystup = pair_retrieval_eval(x, y, x_file, y_file, gold_file, output_file, predict_file, use_gpu=use_gpu)
+
+        with open(result_file, "a") as f:
+            f.write("{},{},{},{},{},{}\n".format(pair, end-start, vystup["precision"], vystup["recall"], vystup["F1"], vystup["best-threshold"]))
+
         vystupy[pair] = {}
-        vystupy[pair]["result"] = vystup
+        for key in vystup.keys(): vystupy[pair][key] = vystup[key]
         vystupy[pair]["time"] = end-start
 
     return vystupy
-
-def evaluate_file(output_file, gold_file, x_file, y_file, predict_file):
-    x_file_id = x_file + ".id"
-    x_file_sent = x_file + ".sent"
-
-    extract_ids_and_sentences(x_file, x_file_id, x_file_sent)
-
-    y_file_id = y_file + ".id"
-    y_file_sent = y_file + ".sent"
-
-    extract_ids_and_sentences(y_file, y_file_id, y_file_sent)
-
-    vystup = bucc_eval(output_file, gold_file, x_file_sent, y_file_sent, x_file_id, y_file_id, predict_file)
-
-    os.remove(x_file_id)
-    os.remove(x_file_sent)
-    os.remove(y_file_id)
-    os.remove(y_file_sent)
-    
-    return vystup
-
-
-def BUCC_eval_folder(input_folder, output_folder):
-    vystupy = {}
-
-    for pair in BUCC_pairs:
-        x_lang = pair.split("-")[0]
-        y_lang = pair.split("-")[1]
-
-        x_file = P.join(input_folder, pair, "{}.training.{}".format(pair, x_lang))
-        y_file = P.join(input_folder, pair, "{}.training.{}".format(pair, y_lang))
-        gold_file =  P.join(input_folder, pair, "{}.training.gold".format(pair))
-        output_file = P.join(output_folder, pair, "{}.training.output".format(pair))
-        predict_file = P.join(output_folder, pair, "{}.training.predict".format(pair))
-
-        vystup = evaluate_file(output_file, gold_file, x_file, y_file, predict_file)
-        vystupy[pair] = vystup
-
-    return vystupy
-
--- evaluation/evaluate_FLORES.py
+++ evaluation/evaluate_FLORES.py
@@ -1,29 +1,93 @@
 import numpy as np
 import os
+import time
 
-from utils_retrieve import extract_file_as_list
+import word_emb
+import labse
+
+import faiss
+
 import os.path as P
 
-def similarity(a: np.array, b: np.array) -> float: #return sim of two vectors
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+from utils_retrieve import knn
 
 # FLORES - accuracy
 # gold labels are (0, 0), (1, 1), ... (i, i), ...
-def accuracy_eval(x: np.array, y: np.array):
+def accuracy_eval(x: np.array, y: np.array, use_gpu):
 
     assert x.shape[0] == y.shape[0], "source and target files have different number of elements"
 
     N = x.shape[0]
 
-    sim_matrix = np.zeros((N, N))
-
-    for i in range(N):
-        for j in range(N):
-            sim_matrix[i, j] = similarity(x[i], y[j])
+    _, ind = knn(x, y, 1, use_gpu)
 
     no_correct = 0
     for i in range(N):
-        if i == np.argmax(sim_matrix[i, :]):
+        if i == ind[i][0]:
             no_correct += 1
         
     return no_correct/N
+
+def FLORES_eval(input_folder, output_folder, extract_emb_f, use_gpu=True):
+
+    if not P.exists(output_folder):
+        os.mkdir(output_folder)
+
+    with open("labse_langs_FLORES.txt", "r") as f:
+        languages = f.readlines()
+    languages = map(lambda x: x.rstrip(), languages)
+
+    languages = list(languages)
+
+    input_folder = P.join(input_folder, "devtest")
+
+    embs = []
+    times = []
+
+    
+    for lang in languages:
+        file = P.join(input_folder, "devtest." + lang)
+        with open(file, encoding="utf-8") as f:
+            sent_list = [l.rstrip("\n") for l in f]
+
+        start = time.time()
+        l_emb = extract_emb_f(sent_list)
+        end = time.time()
+    
+        faiss.normalize_L2(l_emb)
+
+        embs.append(l_emb)
+        times.append(end-start)
+
+        
+    f_times = P.join(output_folder, "time.csv")
+    with open(f_times, "w") as f:
+        f.write("language,time\n")
+        for i in range(len(languages)):
+            f.write("{},{}\n".format(languages[i], times[i]))
+        f.write("all,{}\n".format(sum(times)))
+
+
+    accuracies = []
+    for i in range(len(languages)):
+        accuracies.append([])
+        for j in range(len(languages)):
+            print(languages[i] + " - " + languages[j], flush=True)
+            accuracies[-1].append(accuracy_eval(embs[i], embs[j], use_gpu))
+
+    f_accuracies = P.join(output_folder, "accuracy.csv")
+    with open(f_accuracies, "w") as f:
+        for lang in languages:
+            f.write("," + lang)
+        f.write("\n")
+        for i in range(len(languages)):
+            f.write(languages[i])
+            for j in range(len(languages)):
+                f.write(",")
+                f.write("{:.4f}".format(accuracies[i][j]))
+            f.write("\n")
+
+
+    return languages, times, accuracies
+
+
