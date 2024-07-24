03a270ae6fecb055ca28fd08d9d230ab45e79f5b
Jan Slíva <honzin.sliva@gmail.com>
2024-07-24 23:06:25 +0200

    evaluate FLORES & BUCC at the same time

--- evaluation/evaluate_baseline.py
+++ evaluation/evaluate_baseline.py
@@ -1,34 +1,49 @@
 from labse import Labse
 from word_emb import WordEmb
 from evaluate_BUCC import BUCC_eval
-import json
+from evaluate_FLORES import FLORES_eval
 
 import os.path as P
+import os
+import sys
 
 import argparse
 
 def main():
   parser = argparse.ArgumentParser(description='LaBse evaluation')
   parser.add_argument("--batch_size", type=int, default=100, help="batch size.")
-  parser.add_argument("--BUCC_folder", type=str, default="")
-  parser.add_argument("--word_emb_eval_folder", default=None, type=str)
-  parser.add_argument("--labse_eval_folder",  default=None, type=str)
   parser.add_argument("--use_gpu", type=bool, default=True)
+
+  parser.add_argument("--model", type=str, help="labse|word_emb")
+
+  parser.add_argument("--BUCC_folder", default=None, type=str)
+  parser.add_argument("--FLORES_folder", default=None, type=str)
+  parser.add_argument("--eval_folder", type=str)
   args = parser.parse_args()
 
-  def eval_and_save(folder, model):
-    ret = BUCC_eval(args.BUCC_folder, folder, lambda x: model.predict(x, args.batch_size, False), use_gpu=args.use_gpu)
-    with open(P.join(folder, "results.json"), 'w') as fp:
-      json.dump(ret, fp)
+  if not P.exists(args.eval_folder):
+    os.mkdir(args.eval_folder)
 
-  if (args.word_emb_eval_folder != None):
-    word_emb = WordEmb()
-    eval_and_save(args.word_emb_eval_folder, word_emb)
+  eval_folder = P.join(args.eval_folder, args.model)
+  os.mkdir(eval_folder)
   
-  if (args.labse_eval_folder != None):
-    labse = Labse()
-    eval_and_save(args.labse_eval_folder, labse)
+  if args.model == "labse":
+    model = Labse()
+    pred_f = lambda x: model.predict(x, args.batch_size, False)
+  elif args.model == "word_emb":
+    model = WordEmb()
+    pred_f = lambda x: model.predict(x, args.batch_size, False)
+  else:
+    print(f"Model {args.model} does not exist!", file=sys.stderr)
 
+  if args.BUCC_folder is not None:
+    BUCC_out_f = P.join(eval_folder, "BUCC")
+    BUCC_eval(args.BUCC_folder, BUCC_out_f, pred_f, False, args.use_gpu)
+
+  if args.FLORES_folder is not None:
+    FLORES_out_f = P.join(eval_folder, "FLORES")
+    FLORES_eval(args.FLORES_folder, FLORES_out_f, pred_f, args.use_gpu)
+  
 
 if __name__ == "__main__":
   main()
