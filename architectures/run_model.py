1c3d6422f28a664c7ea2136ae77cd585b4959d6b
Jan Slíva <honzin.sliva@gmail.com>
2024-08-21 22:54:53 +0200

    make model importable

--- architectures/run_model.py
+++ architectures/run_model.py
@@ -13,17 +13,21 @@ batch_size = 128
 epochs = 1
 
 
+def get_model():
+    embs = load_embs(emb_path)
+    args = get_args(**dict)
 
-embs = load_embs(emb_path)
-args = get_args(**dict)
+    return LightConvEncoder(args, None, embs).to("cuda")
 
-light_encoder = LightConvEncoder(args, None, embs).to("cuda")
 
-create_folder(folder)
-create_folder(data_folder)
-save_folder = P.join(folder, "save")
-create_folder(save_folder)
-tb_folder = P.join(folder, "tb")
-create_folder(tb_folder)
+if __name__ == "__main__":
+    create_folder(folder)
+    create_folder(data_folder)
+    save_folder = P.join(folder, "save")
+    create_folder(save_folder)
+    tb_folder = P.join(folder, "tb")
+    create_folder(tb_folder)
 
-architectures.train_model.train(light_encoder, data_folder, save_folder, tb_folder, lr, batch_size, epochs)
+    light_encoder = get_model()
+
+    architectures.train_model.train(light_encoder, data_folder, save_folder, tb_folder, lr, batch_size, epochs)
