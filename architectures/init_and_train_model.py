13b1b2e3b232e7de7df616e229983f2670e62774
Jan Slíva <honzinsliva@gmail.com>
2024-09-30 20:40:57 +0200

    refactor

--- architectures/init_and_train_model.py
+++ architectures/init_and_train_model.py
@@ -2,7 +2,7 @@
 This script intializes the model, the folder structure and runs the distillation training. After each epoch the model is saved in "<save_path>/save/model-<epoch>.pt". The tensorboard logs are saved in "<save_path>/tb/".
 
 usage:
-python init_and_train_model.py --save_path <path_to_save_folder> --data_path <path_to_data_folder> --emb_path <path_to_labse_embs> --lr <learning_rate> --batch_size <batch_size> --epochs <number_of_epochs>
+python init_and_train_model.py --save_path <path_to_save_folder> --data_path <path_to_data_folder> --emb_path <path_to_file_with_labse_emb_matrix> --lr <learning_rate> --batch_size <batch_size> --epochs <number_of_epochs>
 """
 from fairseq.models.lightconv import LightConvEncoder
 import os.path as P
@@ -15,7 +15,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Run the distillation training.')
     parser.add_argument('--save_path', type=str, required=True, help='Path to save the folder')
     parser.add_argument('--data_path', type=str, required=True, help='Path to the data folder')
-    parser.add_argument('--emb_path', type=str, required=True, help='Path to the labse embs')
+    parser.add_argument('--emb_path', type=str, required=True, help='Path to file with word embeddings matrix')
     parser.add_argument('--lr', type=float, help='Learning rate', default=0.0005)
     parser.add_argument('--batch_size', type=int, help='Batch size', default=128)
     parser.add_argument('--epochs', type=int, help='Number of epochs', default=1)
@@ -23,8 +23,10 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_model(emb_path, dict):
+def get_model(emb_path):
     embs = load_embs(emb_path)
+    dict = {"layers" : 1, "kernel_sizes" : [31], "conv_type" : "lightweight", "weight_softmax" : True}
+
     args = get_args(**dict)
 
     return LightConvEncoder(args, None, embs).to("cuda")
@@ -40,8 +42,7 @@ def main():
     batch_size = args.batch_size
     epochs = args.epochs
     percentage = args.percentage
-    
-    dict = {"layers" : 1, "kernel_sizes" : [31], "conv_type" : "lightweight", "weight_softmax" : True}
+
 
     create_folder(folder)
     save_folder = P.join(folder, "save")
@@ -49,7 +50,7 @@ def main():
     tb_folder = P.join(folder, "tb")
     create_folder(tb_folder)
 
-    light_encoder = get_model(emb_path, dict)
+    light_encoder = get_model(emb_path)
 
     architectures.training.train(light_encoder, data_folder, save_folder, tb_folder, lr, batch_size, epochs, percentage)
 
